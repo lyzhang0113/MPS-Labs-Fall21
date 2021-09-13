dad7554e691980fe39cb3afd4698ab354c06814d
gevera1 <gevera@rpi.edu>
2021-09-13 17:59:13 -0400

    setup register ver. of task01

--- LAB-02/src/task01.c
+++ LAB-02/src/task01.c
@@ -13,20 +13,7 @@
 
 /* GPIO PJ0 ---> EXTI0 */   // Register
 /* GPIO JE8 ---> EXTI8 */   // HAL
-
-#define LD1_ON GPIOJ->BSRR = (uint16_t)GPIO_PIN_13
-#define LD1_OFF GPIOJ->BSRR = (uint32_t)GPIO_PIN_13 << 16
-#define LD2_ON GPIOJ->BSRR = (uint16_t)GPIO_PIN_5
-#define LD2_OFF GPIOJ->BSRR = (uint32_t)GPIO_PIN_5 << 16
-#define LD3_ON GPIOA->BSRR = (uint16_t)GPIO_PIN_12
-#define LD3_OFF GPIOA->BSRR = (uint32_t)GPIO_PIN_12 << 16
-#define LD4_ON GPIOD->BSRR = (uint32_t)GPIO_PIN_4 << 16
-#define LD4_OFF GPIOD->BSRR = (uint16_t)GPIO_PIN_4
-
-#define D0_HIGH (GPIOC->IDR & 1 << 7) != 0
-#define D1_HIGH (GPIOC->IDR & 1 << 6) != 0
-#define D2_HIGH (GPIOJ->IDR & 1 << 1) != 0
-#define D3_HIGH (GPIOF->IDR & 1 << 6) != 0
+#define PJ0_HIGH (GPIOJ->IDR & 1) != 0
 
 //------------------------------------------------------------------------------------
 // Includes
@@ -49,30 +36,18 @@ int main(void)
 
     // enable the GPIO port peripheral clock
     __HAL_RCC_GPIOJ_CLK_ENABLE(); 	// Through HAL
-    __HAL_RCC_GPIOA_CLK_ENABLE(); 	// Through HAL
-    __HAL_RCC_GPIOD_CLK_ENABLE(); 	// Through HAL
-    __HAL_RCC_GPIOF_CLK_ENABLE(); 	// Through HAL
-    __HAL_RCC_GPIOC_CLK_ENABLE(); 	// Through HAL
 
     // initialize GPIO ports
-    GPIOA->MODER |= 1 << 12 * 2; // Bitmask for GPIO A, set pin 12 to output
-    GPIOC->MODER &= 0x00; // Bitmask for GPIO C, default setting (input)
-    GPIOJ->MODER |= 1 << 5 * 2; //Bitmask for GPIO J Pin 5 initialization (set it to Output mode): 0x00000400U or 1024U in decimal
-    GPIOJ->MODER |= 1 << 13 * 2; //Bitmask for GPIO J Pin 13 initialization (set it to Output mode)
-    GPIOD->MODER |= 1 << 4 * 2; // Bitmask for GPIO D Pin 4 initialization (set it to Output mode)
-    GPIOF->MODER &= 0x00; // Bitmask for GPIOF, default setting (input)
-
+    GPIOJ->MODER &= 0x00; //Bitmask for GPIO J Pin 0 initialization (set it to Input)
+    
     // set inputs to pull-up
-    GPIOC->PUPDR |= 1 << 7 * 2;// C7
-    GPIOC->PUPDR |= 1 << 6 * 2;// C6
-	GPIOF->PUPDR |= 1 << 6 * 2;// F6
-	GPIOJ->PUPDR |= 1 << 1 * 2;// J1
+  	GPIOJ->PUPDR |= 0x01; // J1
+
+    // enable NVIC ISER for EXTI0 (pos 6)
+    NVIC->ISER0 = (uint32_t) 1 << 6;
 
     while(1)
     {
-    	if (D0_HIGH) LD1_ON; else LD1_OFF;
-    	if (D1_HIGH) LD2_ON; else LD2_OFF;
-    	if (D2_HIGH) LD3_ON; else LD3_OFF;
-    	if (D3_HIGH) LD4_ON; else LD4_OFF;
+
     }
 }
