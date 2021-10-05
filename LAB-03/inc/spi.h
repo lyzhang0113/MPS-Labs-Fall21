fc13e5450c35bce6baf2eb8e26d05d9c27daf2db
Luoyan Zhang <lyzhang0113@gmail.com>
2021-10-05 17:49:45 -0400

    Lab03 SPI functions added

--- LAB-03/inc/spi.h
+++ LAB-03/inc/spi.h
@@ -9,6 +9,14 @@
 
 void initSPI(SPI_HandleTypeDef* hspi);
 
+uint8_t SPI_ReadWriteByte(uint8_t TxData);
 
+uint8_t SPI_ReadByte(SPI_HandleTypeDef* hspi, uint32_t timeout);
+
+void SPI_ReadBytes(SPI_HandleTypeDef* hspi, uint8_t* RxDataBuf, uint32_t length);
+
+void SPI_WriteByte(SPI_HandleTypeDef* hspi, uint8_t TxData, uint32_t timeout);
+
+void SPI_WriteBytes(SPI_HandlTypeDef* hspi, uint8_t* TxDataBuf, uint32_t length);
 
 #endif /* SPI_H_ */
--- LAB-03/src/spi.c
+++ LAB-03/src/spi.c
@@ -32,11 +32,44 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 	}
 }
 
+// SPI Initialization
 void initSPI(SPI_HandleTypeDef* hspi, SPI_TypeDef* Tgt) {
-	hspi->Instance    = Tgt;
-	hspi->Init.Mode   = SPI_MODE_MASTER;
-	hspi->Init.Direction = SPI_DIRECTION_2LINES;
-	hspi->Init.TIMode = SPI_TIMODE_DISABLE;
+	hspi->Instance               = Tgt;
+	hspi->Init.Mode              = SPI_MODE_MASTER;
+	hspi->Init.Direction         = SPI_DIRECTION_2LINES;
+	hspi->Init.DataSize          = SPI_DATASIZE_8BIT;
+	hspi->Init.CLKPolarity       = SPI_POLARITY_HIGH;
+	hspi->Init.CLKPhase          = SPI_PHASE_2EDGE;
+	hspi->Init.NSS               = SPI_NSS_HARD_INPUT;
+	hspi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128; // 108MHz / 128 = 843kHz
+	hspi->Init.FirstBit          = SPI_FIRSTBIT_MSB;
+	hspi->Init.TIMode            = SPI_TIMODE_DISABLE;
 
-	HAL_SPI_Init(&hspi);
+	HAL_SPI_Init(hspi);
+}
+
+// read and write 1 char at the same time since full-duplex
+uint8_t SPI_ReadWriteByte(SPI_HandleTypeDef* hspi, uint8_t TxData) {
+	uint8_t RxData;
+	HAL_SPI_TransmitReceive(hspi, &TxData, &RxData, 1, 1000);
+	return RxData;
+}
+
+uint8_t SPI_ReadByte(SPI_HandleTypeDef* hspi, uint32_t timeout) {
+	// HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+	uint8_t RxData;
+	HAL_SPI_Receive(hspi, &RxData, 1, timeout);
+	return RxData;
+}
+
+void SPI_ReadBytes(SPI_HandleTypeDef* hspi, uint8_t* RxDataBuf, uint32_t length) {
+	HAL_SPI_Receive(hspi, RxDataBuf, length, 1000);
+}
+
+void SPI_WriteByte(SPI_HandleTypeDef* hspi, uint8_t TxData, uint32_t timeout) {
+	HAL_SPI_Transmit(hspi, &TxData, 1, timeout);
+}
+
+void SPI_WriteBytes(SPI_HandlTypeDef* hspi, uint8_t* TxDataBuf, uint32_t length) {
+	HAL_SPI_Transmit(hspi, TxDataBuf, length, 1000);
 }
