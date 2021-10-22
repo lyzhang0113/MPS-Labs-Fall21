f7d1d24e168cde58bfcf71ba642327bb0fcb91f6
Luoyan Zhang <lyzhang0113@gmail.com>
2021-10-21 23:24:50 -0400

    lab4 task 3.1 update

--- LAB-04/src/task03.1.c
+++ LAB-04/src/task03.1.c
@@ -8,8 +8,10 @@
 #include "init.h"
 
 void reset_terminal();
-
-int32_t result_int32;
+int32_t asm_subtask_1();
+int32_t asm_subtask_2(int32_t m1, int32_t m2);
+int32_t asm_subtask_3(int32_t x);
+int32_t asm_subtask_4(int32_t x);
 
 // Main Execution Loop
 int main(void)
@@ -31,13 +33,7 @@ int main(void)
 	printf("***      Subtask 1: Addition      ***\r\n");
 	printf("* 479188563 + 407729234\r\n");
 	printf("* Expected Output: %ld\r\n", (int32_t) 479188563 + (int32_t) 407729234);
-
-	asm volatile ("LDR r3, =0x1C8FD653"); 				// Load 479188563 into r3
-	asm volatile ("LDR r4, =0x184D7452"); 				// Load 407729234 into r4
-	asm volatile ("ADD r3, r3, r4"); 						// r3 = r3 + r4
-	asm("STR r3, %0" : "=m" (result_int32)); 	// Store r3 into result_int32
-
-	printf("* Actual Output:   %ld\r\n", result_int32);
+	printf("* Actual Output:   %ld\r\n", asm_subtask_1());
 	printf("*************************************\r\n\n");
 
 	/*
@@ -49,10 +45,7 @@ int main(void)
 	int32_t m1 = 7501, m2 = -6713;
 	printf("* %ld * %ld\r\n", m1, m2);
 	printf("* Expected Output: %ld\r\n", m1 * m2);
-
-	asm("MUL %[out], %[in1], %[in2]" :[out] "=r" (result_int32) :[in1] "r" (m1), [in2] "r" (m2));
-
-	printf("* Actual Output:   %ld\r\n", result_int32);
+	printf("* Actual Output:   %ld\r\n", asm_subtask_2(m1, m2));
 	printf("*************************************\r\n\n");
 
 	/*
@@ -64,28 +57,12 @@ int main(void)
 	m1 = 4;
 	printf("2 * [%ld] / 3 + 5\r\n", m1);
 	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-
-	asm volatile ("LDR r3, =0x02");
-	asm("MUL %[out], %[in], r3" :[out] "=r" (m1) :[in] "r" (m1));
-	asm volatile ("LDR r3, =0x03");
-	asm("SDIV %[out], %[in], r3" :[out] "=r" (m1) :[in] "r" (m1));
-	asm volatile ("LDR r3, =0x05");
-	asm("ADD %[out], %[in], r3" :[out] "=r" (result_int32) :[in] "r" (m1));
-
-	printf("* Actual Output:   %ld\r\n", result_int32);
+	printf("* Actual Output:   %ld\r\n", asm_subtask_3(m1));
 
 	m1 = 33804;
 	printf("2 * [%ld] / 3 + 5\r\n", m1);
 	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-
-	asm volatile ("LDR r3, =0x02");
-	asm("MUL %[out], %[in], r3" :[out] "=r" (m1) :[in] "r" (m1));
-	asm volatile ("LDR r3, =0x03");
-	asm("SDIV %[out], %[in], r3" :[out] "=r" (m1) :[in] "r" (m1));
-	asm volatile ("LDR r3, =0x05");
-	asm("ADD %[out], %[in], r3" :[out] "=r" (result_int32) :[in] "r" (m1));
-
-	printf("* Actual Output:   %ld\r\n", result_int32);
+	printf("* Actual Output:   %ld\r\n", asm_subtask_3(m1));
 	printf("*************************************\r\n\n");
 
 	/*
@@ -97,22 +74,12 @@ int main(void)
 	m1 = 7;
 	printf("2 * [%ld] / 3 + 5\r\n", m1);
 	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-	asm volatile ("LDR r3, =0x03");
-	asm("SDIV %[out], %[in], r3" :[out] "=r" (m1) :[in] "r" (m1));
-	asm volatile ("LDR r2, =0x02");
-	asm volatile ("LDR r3, =0x05");
-	asm("MLA %[out], %[in], r2, r3" :[out] "=r" (result_int32) :[in] "r" (m1));
-	printf("* Actual Output:   %ld\r\n", result_int32);
+	printf("* Actual Output:   %ld\r\n", asm_subtask_4(m1));
 
 	m1 = 49125;
 	printf("2 * [%ld] / 3 + 5\r\n", m1);
 	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-	asm volatile ("LDR r3, =0x03");
-	asm("SDIV %[out], %[in], r3" :[out] "=r" (m1) :[in] "r" (m1));
-	asm volatile ("LDR r2, =0x02");
-	asm volatile ("LDR r3, =0x05");
-	asm("MLA %[out], %[in], r2, r3" :[out] "=r" (result_int32) :[in] "r" (m1));
-	printf("* Actual Output:   %ld\r\n", result_int32);
+	printf("* Actual Output:   %ld\r\n", asm_subtask_4(m1));
 
 	printf("*************************************\r\n\n");
 
@@ -121,4 +88,34 @@ int main(void)
 void reset_terminal() {
     printf("\033[0m\033[2J\033[;H\033[r"); // Erase screen & move cursor to home position
     fflush(stdout); // Need to flush stdout after using printf that doesn't end in \n
+}
+
+int32_t asm_subtask_1() {
+	int32_t res = 0;
+	// Load 479188563 into r3 & Load 407729234 into r4, then add r3 = r3 + r4
+	asm volatile ("LDR r3, =0x1C8FD653 \r\n LDR r4, =0x184D7452 \r\n ADD r3, r3, r4");
+	asm("STR r3, %0" : "=m" (res)); 	// Store r3 into res
+	return res;
+}
+
+int32_t asm_subtask_2(int32_t m1, int32_t m2) {
+	int32_t res = 0;
+	asm("MUL %[out], %[in1], %[in2]" :[out] "=r" (res) :[in1] "r" (m1), [in2] "r" (m2));
+	return res;
+}
+
+int32_t asm_subtask_3(int32_t x) {
+	int32_t res = 0;
+	asm volatile ("LDR r3, =0x02 \r\n MUL r2, %[in], r3" : :[in] "r" (x));
+	asm volatile ("LDR r3, =0x03 \r\n SDIV r1, r2, r3");
+	asm volatile ("LDR r3, =0x05 \r\n ADD %[out], r1, r3" :[out] "=r" (res));
+	return res;
+}
+
+int32_t asm_subtask_4(int32_t x) {
+	int32_t res = 0;
+	asm volatile ("LDR r3, =0x03 \r\n SDIV r2, %[in], r3" : :[in] "r" (x));
+	asm volatile ("LDR r3, =0x02 \r\n LDR r4, =0x05" : : : "r4");
+	asm("MLA %[out], r2, r3, r4" :[out] "=r" (res));
+	return res;
 }
