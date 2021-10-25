2d7546233ddbfde178dc1edf968a9be785a2239c
gevera1 <gevera@rpi.edu>
2021-10-25 18:19:28 -0400

    Finished Task 3.2

--- LAB-04/src/task03.2.c
+++ LAB-04/src/task03.2.c
@@ -8,10 +8,10 @@
 #include "init.h"
 
 void reset_terminal();
-int32_t asm_subtask_1();
-int32_t asm_subtask_2(int32_t m1, int32_t m2);
-int32_t asm_subtask_3(int32_t x);
-int32_t asm_subtask_4(int32_t x);
+float asm_subtask_1();
+float asm_subtask_2(float m1, float m2);
+float asm_subtask_3(float x);
+float asm_subtask_4(float x);
 
 // Main Execution Loop
 int main(void)
@@ -31,55 +31,59 @@ int main(void)
 	 * 		Print Result
 	 */
 	printf("***      Subtask 1: Addition      ***\r\n");
-	printf("* 479188563 + 407729234\r\n");
-	printf("* Expected Output: %ld\r\n", (int32_t) 479188563 + (int32_t) 407729234);
-	printf("* Actual Output:   %ld\r\n", asm_subtask_1());
+	printf("* 47.83 + 40.734\r\n");
+	printf("* Expected Output: %f\r\n", (float) 41.83f + (float) 40.734f);
+	printf("* Actual Output:   %f\r\n", asm_subtask_1());
 	printf("*************************************\r\n\n");
 
 	/*
 	 * 2. 	Use Extended Assembly:
-	 * 			Multiply two int32_t variables (signed long)
+	 * 			Multiply two single precision float variables
 	 * 		Print Result
 	 */
 	printf("***   Subtask 2: Multiplication   ***\r\n");
-	int32_t m1 = 7501, m2 = -6713;
-	printf("* %ld * %ld\r\n", m1, m2);
-	printf("* Expected Output: %ld\r\n", m1 * m2);
-	printf("* Actual Output:   %ld\r\n", asm_subtask_2(m1, m2));
+	float m1 = 7.501f, m2 = -67.13f;
+	printf("* %f * %f\r\n", m1, m2);
+	printf("* Expected Output: %f\r\n", (float)m1 * m2);
+	printf("* Actual Output:   %f\r\n", asm_subtask_2(m1, m2));
 	printf("*************************************\r\n\n");
 
 	/*
 	 * 3. 	Use Extended Assembly:
-	 * 			Evaluate 2 * x / 3 + 5 using 32-bit integer math
+	 * 			Evaluate 2 * x / 3 + 5 using floating point mathematics
 	 * 		Print Result
 	 */
 	printf("***  Subtask 3: ADD, MUL, and DIV ***\r\n");
-	m1 = 4;
-	printf("2 * [%ld] / 3 + 5\r\n", m1);
-	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-	printf("* Actual Output:   %ld\r\n", asm_subtask_3(m1));
+	m1 = 4.0f;
+	printf("2 * [%f] / 3 + 5\r\n", m1);
+	printf("* Expected Output: %f\r\n", (float)2 * m1 / 3 + 5);
+	printf("* Actual Output:   %f\r\n", asm_subtask_3(m1));
 
-	m1 = 33804;
-	printf("2 * [%ld] / 3 + 5\r\n", m1);
-	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-	printf("* Actual Output:   %ld\r\n", asm_subtask_3(m1));
+	printf("\r\n");
+
+	m1 = 384.0f;
+	printf("2 * [%f] / 3 + 5\r\n", m1);
+	printf("* Expected Output: %f\r\n", (float)2 * m1 / 3 + 5);
+	printf("* Actual Output:   %f\r\n", asm_subtask_3(m1));
 	printf("*************************************\r\n\n");
 
 	/*
 	 * 4. 	Use Extended Assembly:
-	 * 			Evaluate 2 * x / 3 + 5 using 32-bit integer math using MAC
+	 * 			Evaluate 2 * x / 3 + 5 using floating point math using MAC
 	 * 		Print Result
 	 */
 	printf("***   Subtask 4: Math Using MAC   ***\r\n");
-	m1 = 7;
-	printf("2 * [%ld] / 3 + 5\r\n", m1);
-	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-	printf("* Actual Output:   %ld\r\n", asm_subtask_4(m1));
+	m1 = 7.0f;
+	printf("2 * [%f] / 3 + 5\r\n", m1);
+	printf("* Expected Output: %f\r\n", 2 * m1 / 3 + 5);
+	printf("* Actual Output:   %f\r\n", asm_subtask_4(m1));
 
-	m1 = 49125;
-	printf("2 * [%ld] / 3 + 5\r\n", m1);
-	printf("* Expected Output: %ld\r\n", 2 * m1 / 3 + 5);
-	printf("* Actual Output:   %ld\r\n", asm_subtask_4(m1));
+	printf("\r\n");
+
+	m1 = 49.125f;
+	printf("2 * [%f] / 3 + 5\r\n", m1);
+	printf("* Expected Output: %f\r\n", 2 * m1 / 3 + 5);
+	printf("* Actual Output:   %f\r\n", asm_subtask_4(m1));
 
 	printf("*************************************\r\n\n");
 
@@ -90,31 +94,33 @@ void reset_terminal() {
     fflush(stdout); // Need to flush stdout after using printf that doesn't end in \n
 }
 
-int32_t asm_subtask_1() {
-	int32_t res = 0;
-	// Load 479188563 into r3 & Load 407729234 into r4, then add r3 = r3 + r4
-	asm volatile ("LDR r3, =0x1C8FD653 \r\n LDR r4, =0x184D7452 \r\n ADD r3, r3, r4");
-	asm("STR r3, %0" : "=m" (res)); 	// Store r3 into res
+float asm_subtask_1() {
+	float res = 0;
+	// Load 41.83 into r3 & Load 40.734 into r4, then add r3 = r3 + r4
+	asm volatile ("VLDR.F32 s0,=0x422751EC \r\n VLDR.F32 s1,=0x4222EF9E \r\n VADD.F32 s0, s0, s1");
+	asm("VSTR s0, %0" : "=m" (res)); 	// Store s3 into res
+	return (float) res;
+}
+
+float asm_subtask_2( float m1, float m2 ) {
+	float res = 0;
+	asm("VMUL.F32 %[out], %[in1], %[in2]" :[out] "=t" (res) :[in1] "t" (m1), [in2] "t" (m2));
 	return res;
 }
 
-int32_t asm_subtask_2(int32_t m1, int32_t m2) {
-	int32_t res = 0;
-	asm("MUL %[out], %[in1], %[in2]" :[out] "=r" (res) :[in1] "r" (m1), [in2] "r" (m2));
+float asm_subtask_3( float x ) {
+	float res = 0;
+	asm volatile ("VLDR.F32 s3, =0x40000000 \r\n VMUL.F32 s2, %[in], s3" : :[in] "t" (x));
+	asm volatile ("VLDR.F32 s3, =0x40400000 \r\n VDIV.F32 s1, s2, s3");
+	asm volatile ("VLDR.F32 s3, =0x40A00000 \r\n VADD.F32 %[out], s1, s3" :[out] "=t" (res));
 	return res;
 }
 
-int32_t asm_subtask_3(int32_t x) {
-	int32_t res = 0;
-	asm volatile ("LDR r3, =0x02 \r\n MUL r2, %[in], r3" : :[in] "r" (x));
-	asm volatile ("LDR r3, =0x03 \r\n SDIV r1, r2, r3");
-	asm volatile ("LDR r3, =0x05 \r\n ADD %[out], r1, r3" :[out] "=r" (res));
-	return res;
-}
-
-int32_t asm_subtask_4(int32_t x) {
-	int32_t res = 0;
-	asm volatile ("LDR r3, =0x03 \r\n SDIV r2, %[in], r3" : :[in] "r" (x));
-	asm volatile ("LDR r3, =0x02 \r\n LDR r4, =0x05 \r\n MLA %[out], r2, r3, r4" :[out] "=r" (res) : : "r4");
+float asm_subtask_4( float x ) {
+	float res = 0;
+	asm volatile ("VLDR.F32 s1, %[in]" : : [in] "m" (x));	// Add in report
+	asm volatile ("VLDR.F32 s3, =0x3F2AAAAB \r\n VLDR.F32 s4, =0x40A00000");
+	asm volatile ("VMLA.F32 s4, s1, s3");
+	asm volatile ("VSTR s4, %0" : "=m" (res));
 	return res;
 }
