551389c40bb0ac811c511f2d301ca7aedc226a1c
gevera1 <gevera@rpi.edu>
2021-11-08 19:19:27 -0500

    Finished uint8_t in Task 1

--- LAB-05/src/task01.c
+++ LAB-05/src/task01.c
@@ -1,3 +1,16 @@
+/*
+ * 8-bit DMA 	- CH1 Stream 0
+ * 16-bit DMA 	- CH3 Stream 1
+ * 32-bit DMA	- CH2 Stream 2
+ *
+ * ERRORS:
+ * 	- Having to keep printing within callback
+ * 	- Adding Abort to end of callback
+ * 	- infinite while loop after HAL_DMA_START so that program doesn't terminate
+ * 	- Weird printing from callback
+ *
+ */
+
 #include "init.h" // Always need init.h, otherwise nothing will work.
 #include<stdint.h>
 #include<stdlib.h>
@@ -14,9 +27,15 @@ uint32_t		c1[10],		 c2[100], 		c3[1000];
 
 uint32_t cycles;
 
-DMA_HandleTypeDef hdma2_8b;
+DMA_HandleTypeDef hdma2_8b, hdma2_16b, hdma2_32b;
 
 int err_handler( void );
+void HAL_DMA2_XferCpltCallback( DMA_HandleTypeDef * hdma );
+
+void Terminal_Init() {
+    printf("\033[0m\033[2J\033[;H"); // Erase screen & move cursor to home position
+    fflush(stdout); // Need to flush stdout after using printf that doesn't end in \n
+}
 
 void Data_Init( void )
 {
@@ -30,7 +49,7 @@ void Data_Init( void )
 		u16_100[j] = (uint16_t) j;
 		u32_100[j] = (uint32_t) j;
 	}
-	for (uint8_t k = 0; k < 1000; k++) {
+	for (uint32_t k = 0; k < 1000; k++) {
 		 u8_1000[k] = ( uint8_t) k/4;
 		u16_1000[k] = (uint16_t) k/2;
 		u32_1000[k] = (uint32_t) k;
@@ -41,48 +60,87 @@ void DMA_Init( void )
 {
 	DMA_InitTypeDef initDMA;
 
-	HAL_DMA_Abort(&hdma2_8b);
-
 	__DMA2_CLK_ENABLE();
 
-	initDMA.Channel 			= DMA_CHANNEL_0;
+	initDMA.Channel 			= DMA_CHANNEL_1;
 	initDMA.Direction			= DMA_MEMORY_TO_MEMORY;
 	initDMA.PeriphInc 			= DMA_PINC_DISABLE;
 	initDMA.MemInc				= DMA_MINC_ENABLE;
 	initDMA.Mode				= DMA_NORMAL;
 	initDMA.MemDataAlignment	= DMA_MDATAALIGN_BYTE;
-	initDMA.FIFOMode			= DMA_FIFOMODE_ENABLE;
+	initDMA.FIFOMode			= DMA_FIFOMODE_DISABLE;
+	initDMA.Priority			= DMA_PRIORITY_VERY_HIGH;
 
-	hdma2_8b.Instance 			= DMA2_Stream1;
+	hdma2_8b.Instance 			= DMA2_Stream0;
 	hdma2_8b.Init 				= initDMA;
+	hdma2_8b.XferCpltCallback	= &HAL_DMA2_XferCpltCallback;
 
 	if (HAL_DMA_Init(&hdma2_8b) != HAL_OK)
 		err_handler();
 
-	HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
+	HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ	(DMA2_Stream0_IRQn);
+
 }
 
-void DMA_IRQHandler( void )
+void DMA2_Stream0_IRQHandler( void )
 {
 	HAL_DMA_IRQHandler(&hdma2_8b);
 }
 
-void HAL_DMA_XferCpltCallback( DMA_HandleTypeDef * hdma )
+void HAL_DMA2_XferCpltCallback( DMA_HandleTypeDef * hdma )
 {
 	cycles = DWT->CYCCNT;
+	printf("DMA Implementation: %ld cycles\r\n", cycles);
 
-	switch (hdma->Instance)
-	{
-	case (DMA2_Stream1):
-	case (DMA2_Stream3):
-	case (DMA2_Stream5):
-	}
+	cycles = 0;
+	HAL_DMA_Abort_IT(hdma);
+}
+
+void test8bit( void )
+{
+	uint8_t sw_buf1[10], sw_buf2[100], sw_buf3[1000];
+
+	printf("\033[4;1H*********** Unsigned 8-bit Integers (uint8_t) ***********\r\n");
+
+	printf("Moving arrays of Size 10 -\r\n");
+
+	DWT->CYCCNT = 0;
+	memcpy(sw_buf1, u8_10, 10*sizeof(uint8_t));
+	cycles = DWT->CYCCNT;
+	printf("\tSofware Implementation: %ld cycles\r\n", cycles);
+
+	cycles = 0;
+	HAL_Delay(1000);
+
+	DWT->CYCCNT = 0;
+	HAL_DMA_Start_IT(&hdma2_8b, u8_10, a1, 10 * sizeof(uint8_t));
+
+	while (DMA_NOT_DONE);
+
+	printf("Moving arrays of Size 100 -\r\n");
+
+	DWT->CYCCNT = 0;
+	memcpy(sw_buf2, u8_100, 100*sizeof(uint8_t));
+	cycles = DWT->CYCCNT;
+	printf("\tSofware Implementation: %ld cycles\r\n", cycles);
+
+	cycles = 0;
+	HAL_Delay(1000);
+
+	DWT->CYCCNT = 0;
+	HAL_DMA_Start_IT(&hdma2_8b, u8_100, a2, 100 * sizeof(uint8_t));
+	while (DMA_NOT_DONE);
+
+	printf("\tDMA Implementation: %ld cycles\r\n", cycles);
+
+	cycles = 0;
 }
 
 int main(void)
 {
 	Sys_Init();
+	Terminal_Init();
 	Data_Init();
 	DMA_Init();
 
@@ -105,14 +163,14 @@ int main(void)
 	printf("And printing the above took %lu CPU cycles\r\n",cycles);
 	fflush(stdout);
 
-	DWT->CYCCNT = 0;
-	HAL_DMA_Start_IT(&hdma2_8b, u8_10[0], a1[0], 10*sizeof(uint8_t));
+	test8bit();
 
-	DWT->CYCCNT = 0;
-	HAL_DMA_Start_IT(&hdma2_16b, u16_10[0], b1[0], 10*sizeof(uint16_t));
-
-	DWT->CYCCNT = 0;
-	HAL_DMA_Start_IT(&hdma2_32b, u32_10[0], c1[0], 10*sizeof(uint32_t));
+//	DWT->CYCCNT = 0;
+//	HAL_DMA_Start_IT(&hdma2_16b, u16_10[0], b1[0], 10*sizeof(uint16_t));
+//
+//	DWT->CYCCNT = 0;
+//	HAL_DMA_Start_IT(&hdma2_32b, u32_10[0], c1[0], 10*sizeof(uint32_t));
+	while (1);
 }
 
 int err_handler( void )
