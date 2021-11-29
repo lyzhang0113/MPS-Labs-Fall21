46659f156348adf37d066cf6780a8a68068e46df
Luoyan Zhang <zly2000@hotmail.com>
2021-11-29 17:22:15 -0500

    Lab6 Task1 only middle button work

--- LAB-06-USB/src/task01.c
+++ LAB-06-USB/src/task01.c
@@ -14,7 +14,9 @@
 #include "usbh_diskio.h"
 
 USBH_HandleTypeDef husbh;
+HID_MOUSE_Info_TypeDef* mouse_info;
 
+uint8_t poll_interval;
 uint8_t CONNECTED = 0;
 
 void Term_Init();
@@ -35,28 +37,45 @@ int main(void){
 	USBH_Start(&husbh);
 	while(1){
 		USBH_Process(&husbh);
-		// Other stuff
 	}
 }
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
 	switch (id) {
 	case HOST_USER_SELECT_CONFIGURATION:
+		printf("HOST_USER_SELECT_CONFIGURATION\r\n");
 		break;
 	case HOST_USER_CLASS_ACTIVE:
+		printf("HOST_USER_CLASS_ACTIVE\r\n");
 		// A device has been attached and enumerated and is ready to use
 		CONNECTED = 1;
+		// Init Mouse
+//		if (USBH_HID_MouseInit(phost) != USBH_OK) {
+//			printf("ERROR: Mouse Initialization\r\n");
+//		}
 		break;
 	case HOST_USER_CLASS_SELECTED:
+		printf("HOST_USER_CLASS_SELECTED\r\n");
 		break;
 	case HOST_USER_CONNECTION:
+		printf("HOST_USER_CONNECTION\r\n");
 		break;
 	case HOST_USER_DISCONNECTION:
+		Term_Init();
+		printf("HOST_USER_DISCONNECTION\r\n");
 		CONNECTED = 0;
 		break;
 	case HOST_USER_UNRECOVERED_ERROR:
+		printf("HOST_USER_UNRECOVERED_ERROR\r\n");
 		break;
 	}
+}
+
+void USBH_HID_EventCallback(USBH_HandleTypeDef *phost) {
+	mouse_info = USBH_HID_GetMouseInfo(&husbh);
+	printf("\rx: %d; y: %d; %d %d %d", mouse_info->x, mouse_info->y, mouse_info->buttons[0], mouse_info->buttons[1], mouse_info->buttons[2]);
+	fflush(stdout);
+
 }
 
 void Term_Init(void) {
