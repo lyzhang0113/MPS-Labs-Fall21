ebdf8e553e6d656af98103d2ad6930d3d01b2195
Luoyan Zhang <zly2000@hotmail.com>
2021-12-02 18:10:34 -0500

    Final LAB BT connection added

--- LAB-FINAL/src/controller.c
+++ LAB-FINAL/src/controller.c
@@ -3,12 +3,12 @@
 //--------------------------------
 //
 //
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 100
 #include "init.h"
 
+void BT_Connect();
+char BT_TransmitReceive(char c);
 void Term_Init(void);
-void uart_read(UART_HandleTypeDef *huart, uint32_t timeout);
-void print_buf(char* buf, uint32_t size);
 
 char uart_rx[BUFFER_SIZE] = {0};
 UART_HandleTypeDef uart6;
@@ -18,22 +18,7 @@ int main(void) {
 	Sys_Init();
 	Term_Init();
 	initUart(&uart6, 9600, USART6);
-	HAL_Delay(1000);
-
-	uart_print(&uart6, "AT+VERSION");
-	uart_read(&uart6, 100);
-	printf("Bluetooth Module Status: \r\n");
-	print_buf(uart_rx, BUFFER_SIZE);
-	printf("Complete");
-
-	HAL_Delay(1000);
-
-	uart_print(&uart6, "AT+ROLE=M");
-	uart_read(&uart6, 100);
-	printf("Bluetooth Module Set To Master: \r\n");
-	print_buf(uart_rx, BUFFER_SIZE);
-
-	printf("Complete");
+	BT_Connect();
 }
 
 
@@ -43,15 +28,17 @@ void Term_Init(void)
     fflush(stdout); // Need to flush stdout after using printf that doesn't end in \n
 }
 
-void uart_read(UART_HandleTypeDef *huart, uint32_t timeout) {
-	HAL_UART_Receive(huart, (uint8_t *)uart_rx, BUFFER_SIZE, timeout);
+char BT_TransmitReceive(char c) {
+	char rx[1] = {NULL};
+	HAL_UART_Transmit(&uart6, &c, 1, 100);
+	HAL_UART_Receive(&uart6, (uint8_t *)rx, 1, 100);
+	return (char) rx[0];
 }
 
-void print_buf(char* buf, uint32_t size) {
-	for (uint32_t i = 0; i < size; i++) {
-		if (buf[i] == NULL || buf[i] == 0) break;
-		printf("%c", buf[i]);
-		buf[i] = NULL;
+void BT_Connect() {
+	while (1) {
+		HAL_Delay(1000);
+		if (BT_TransmitReceive('p') == 'p') break;
 	}
-	fflush(stdout);
+	printf("Connected\r\n");
 }
